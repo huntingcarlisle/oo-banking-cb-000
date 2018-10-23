ece6adfae6a9f27add6da45f743d78eccabc4567
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-23 18:06:48 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
   
   def execute_transaction
-    unless @status = "complete":
+    unless @status = "complete"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
